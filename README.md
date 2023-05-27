ed253e006f5636f886628775b567653b0683151c
njindal <njindal@adobe.com>
2023-05-27 14:57:46 +0530

    Add application file

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
----
-title: Test
-emoji: 🌍
-colorFrom: yellow
-colorTo: indigo
-sdk: gradio
-sdk_version: 3.32.0
-app_file: app.py
-pinned: false
----
-
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+# serverless-wav2lip
---
+++ app.py
@@ -0,0 +1,44 @@
+import gradio as gr
+from yt_dlp import YoutubeDL
+import os
+import subprocess
+    
+def download_video(url):
+  ydl_opts = {'overwrites':True, 'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4', 'outtmpl':'/content/video.mp4'}
+  with YoutubeDL(ydl_opts) as ydl:
+    ydl.download(url)
+    return f"/content/video.mp4"
+
+def generate(audio_in, video_in):
+    print(audio_in, video_in)
+    if video_in is not None:
+      command = f"python Wav2Lip/inference.py --checkpoint_path Wav2Lip/checkpoints/wav2lip_gan.pth --face '{video_in}' --audio '{audio_in}'"
+    else:
+      command = f"python Wav2Lip/inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face '/content/video.mp4' --audio '{audio_in}'"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    print("stdout:", result.stdout)
+    print("stderr:", result.stderr)
+
+    # os.system(f"python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face '/content/video.mp4' --audio '{audio_in}'")
+    return f"Wav2Lip/results/result_voice.mp4"
+
+app = gr.Blocks()
+with app:
+  with gr.Row():
+    with gr.Column():
+      input_text = gr.Textbox(show_label=False, value="https://www.youtube.com/watch?v=FAyKDaXEAgc")
+      input_download_button = gr.Button(value="Download from YouTube or Twitch")
+      video_in = gr.Video(show_label=False, type='filepath')
+      audio_in = gr.Audio(show_label=False, type='filepath')
+      input_generate_button = gr.Button(value="Generate")
+    with gr.Column():
+        video_out = gr.Video(label="Output Video")
+    input_download_button.click(download_video, inputs=[input_text], outputs=[video_out])
+    input_generate_button.click(generate, inputs=[audio_in, video_in], outputs=[video_out])
+  
+# app.queue().launch(debug=True, share=True)
+
+# run the app
+# app.launch(server_port=8080, enable_queue=False)
+app.launch()
